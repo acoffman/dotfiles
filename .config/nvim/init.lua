805559bd17f1a7fb887cbec0ff9715bb91b1fca8
Adam Coffman <acc@fastmail.com>
2022-01-04 11:04:18 -0600

    Update init.lua
    switch from deprecated compe to coq for completion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,7 +22,7 @@ require('packer').startup(function()
   use 'itchyny/lightline.vim'
   use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'} }
   use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-compe'
+  use { 'ms-jpq/coq_nvim', branch = 'coq' }
   use 'RRethy/nvim-base16'
   use 'broadinstitute/vim-wdl'
   use 'godlygeek/tabular'
@@ -133,6 +133,14 @@ vim.api.nvim_set_keymap('n', '<leader>r', [[<cmd>lua require('telescope.builtin'
 vim.api.nvim_set_keymap('n', '<leader>,', [[<cmd>lua require('telescope.builtin').lsp_code_actions()<cr>]], { noremap = true, silent = true})
 
 
+-- COQ config (also in individual language server config)
+vim.g.coq_settings = {
+ auto_start = 'shut-up',
+ ['clients.snippets.warn'] = {},
+ ['clients.tmux.enabled'] = false
+}
+
+local coq = require "coq"
 
 -- LSP settings
 local nvim_lsp = require('lspconfig')
@@ -143,7 +151,7 @@ local on_attach = function(_client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
@@ -157,19 +165,25 @@ end
 --vim.lsp.set_log_level("debug")
 
 -- Enable the language servers
-nvim_lsp.angularls.setup{
+nvim_lsp.angularls.setup(
+  coq.lsp_ensure_capabilities({
   on_attach = on_attach,
   filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx" },
   root_dir = nvim_lsp.util.root_pattern("angular.json")
-}
-nvim_lsp.tsserver.setup{
-  on_attach = on_attach
-}
-nvim_lsp.graphql.setup{
-  on_attach = on_attach
-}
+}))
 
-nvim_lsp.solargraph.setup {
+nvim_lsp.tsserver.setup(
+coq.lsp_ensure_capabilities({
+  on_attach = on_attach
+}))
+
+nvim_lsp.graphql.setup(
+coq.lsp_ensure_capabilities({
+  on_attach = on_attach
+}))
+
+nvim_lsp.solargraph.setup(
+coq.lsp_ensure_capabilities({
   on_attach = on_attach,
   cmd = { "solargraph", "stdio" },
   filetypes = { "ruby" },
@@ -177,73 +191,16 @@ nvim_lsp.solargraph.setup {
     settings = {
       solargraph = {
         --rubocop
-        diagnostics = true
+        diagnostics = false
     }
   }
-}
+}))
+
 
 -- Map :Format to vim.lsp.buf.formatting()
 vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
 
 
--- Compe setup
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = true;
-
-  source = {
-    path = true;
-    nvim_lsp = true;
-  };
-}
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
-end
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  else
-    return t "<S-Tab>"
-  end
-end
-
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 -- Show lightbulb when code action is available
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
