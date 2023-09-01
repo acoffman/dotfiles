45dcb7fe093c950851f2f674d83f2eba626010e4
Adam Coffman <acc@fastmail.com>
2023-09-01 09:48:08 -0500

    Add fzf-native dep for telescope

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -130,7 +130,11 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     dependencies = {
-      'nvim-lua/plenary.nvim'
+      'nvim-lua/plenary.nvim',
+      { 
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make' 
+      }
     },
     config = function()
       require 'config.telescope'
