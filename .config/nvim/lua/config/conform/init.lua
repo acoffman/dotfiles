c53b429727dddead726fcb9c2017fab05fa82c33
Adam Coffman <acoffman@wustl.edu>
2023-11-27 19:21:51 -0600

    autoformat html

--- .config/nvim/lua/config/conform/init.lua
+++ .config/nvim/lua/config/conform/init.lua
@@ -2,7 +2,8 @@ local conform = require("conform")
 
 conform.setup({
   formatters_by_ft = {
-    javascript = { "prettierd" }
+    javascript = { "prettierd" },
+    html = { "prettierd" },
   },
   format_on_save = {
     timeout_ms = 500,
