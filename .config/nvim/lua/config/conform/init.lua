da82efe66d78d847e9ea10201f489a26f695ca92
Adam Coffman <acc@fastmail.com>
2024-03-15 16:58:22 -0500

    format react files

--- .config/nvim/lua/config/conform/init.lua
+++ .config/nvim/lua/config/conform/init.lua
@@ -7,6 +7,7 @@ conform.setup({
     angular = { "prettier" },
     typescript = { "prettierd" },
     lua = { "stylua" },
+    typescriptreact = { "prettier" },
   },
   format_on_save = {
     timeout_ms = 500,
