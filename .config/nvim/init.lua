154c043c4d546d71837b6227abf6ddc95c62d29a
Adam Coffman <acc@fastmail.com>
2024-09-23 08:57:02 -0500

    updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 --Remap leader to , needs to happen before lazy init
 vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 require("lazy").setup("config.plugins", {
   performance = {
